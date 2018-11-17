28f36e75880d96284e408bdebeb30bcdcc3ccd94
carissa morrow <carissamorrow@hotmail.com>
2018-11-17 13:04:04 -0700

    possiblefix

--- app/components/quote/quote-service.js
+++ app/components/quote/quote-service.js
@@ -1,11 +1,10 @@
-
+import quote from "../../models/quote.js"
 
 let url = '//bcw-getter.herokuapp.com/?url=';
 let url2 = 'https://favqs.com/api/qotd';
 let apiUrl = url + encodeURIComponent(url2);
 
 //Do Not Edit above we have to go through the bcw-getter to access this api
-
 
 
 // @ts-ignore
@@ -19,6 +18,7 @@ export default class QuoteService {
 	getQuote(callWhenDone) {
 		console.log('looking for some good quotes')
 		quoteApi().then((res) => {
+			let Quote = new quote(res.data)
 			callWhenDone(res.data.quote)
 		})
 	}
